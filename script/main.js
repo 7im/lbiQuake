a48cfcea45e60454cddd346e9727525f144c9928
Tim <tim2_8@hotmail.com>
2013-04-03 09:51:01 +0100

    game file structure

---
+++ script/main.js
@@ -0,0 +1,301 @@
+/*!
+ *
+ *   melonJS
+ *   http://www.melonjs.org
+ *
+ *   Step by step game creation tutorial
+ *
+ **/
+
+// game resources
+var g_resources = [{
+    name: "area01_level_tiles",
+    type: "image",
+    src: "data/maps_tileset/area01_level_tiles.png"
+}, {
+    name: "metatiles32x32",
+    type: "image",
+    src: "data/maps_tileset/metatiles32x32.png"
+}, {
+    name: "level01",
+    type: "tmx",
+    src: "data/level01.tmx"
+}, {
+    name: "area01",
+    type: "tmx",
+    src: "data/area01.tmx"
+}, {
+    name: "area02",
+    type: "tmx",
+    src: "data/area02.tmx"
+}, {
+    name: "gripe_run_right",
+    type: "image",
+    src: "data/sprite/gripe_run_right.png"
+},
+// the title screen
+{
+	name: "title_screen",
+	type: "image",
+	src: "data/GUI/title_screen.png"
+},
+// the parallax background
+{
+    name: "parallax1",
+    type: "image",
+    src: "data/background_images/parallax1.png"
+}, {
+    name: "parallax2",
+    type: "image",
+    src: "data/background_images/parallax2.png"
+},
+// the laser
+{
+    name: "spinning_coin_gold",
+    type: "image",
+    src: "data/sprite/spinning_coin_gold.png"
+},
+// the spinning coin spritesheet
+{
+    name:"laser_light",
+    type: "image",
+    src:"data/sprite/tmplaser.png"
+},
+// our enemty entity
+{
+    name: "wheelie_right",
+    type: "image",
+    src: "data/sprite/wheelie_right.png"
+},
+// game font
+{
+    name: "32x32_font",
+    type: "image",
+    src: "data/sprite/32x32_font.png"
+},
+
+// audio resources
+{
+    name: "cling",
+    type: "audio",
+    src: "data/audio/",
+    channel: 2
+}, {
+    name: "stomp",
+    type: "audio",
+    src: "data/audio/",
+    channel: 1
+}, {
+    name: "jump",
+    type: "audio",
+    src: "data/audio/",
+    channel: 1
+}, {
+    name: "DST-InertExponent",
+    type: "audio",
+    src: "data/audio/",
+    channel: 1
+}];
+
+
+var jsApp	=
+{
+	/* ---
+
+		Initialize the jsApp
+
+		---			*/
+	onload: function()
+	{
+
+		// init the video
+		if (!me.video.init('jsapp', 640, 480, false, 1.0))
+		{
+			alert("Sorry but your browser does not support html 5 canvas.");
+         return;
+		}
+
+		// Global settings
+		me.sys.gravity = 0;
+
+		// Debugging settings
+		me.debug.renderHitBox = true;
+
+
+
+		// initialize the "audio"
+		me.audio.init("mp3,ogg");
+
+		// set all resources to be loaded
+		me.loader.onload = this.loaded.bind(this);
+
+		// set all resources to be loaded
+		me.loader.preload(g_resources);
+
+		// load everything & display a loading screen
+		me.state.change(me.state.LOADING);
+	},
+
+
+	/* ---
+
+		callback when everything is loaded
+
+		---										*/
+	loaded: function () {
+		// set the "Play/Ingame" Screen Object
+		me.state.set(me.state.MENU, new TitleScreen());
+
+		// set the "Play/Ingame" Screen Object
+		me.state.set(me.state.PLAY, new PlayScreen());
+
+		// add our player entity in the entity pool
+		me.entityPool.add("mainPlayer", PlayerEntity);
+		me.entityPool.add("CoinEntity", CoinEntity);
+		me.entityPool.add("EnemyEntity", EnemyEntity);
+
+		// enable the keyboard
+		me.input.bindKey(me.input.KEY.LEFT,  "left");
+		me.input.bindKey(me.input.KEY.RIGHT, "right");
+		me.input.bindKey(me.input.KEY.UP,    "up");
+        me.input.bindKey(me.input.KEY.DOWN,  "down");
+		me.input.bindKey(me.input.KEY.SPACE,  "shoot");
+		// me.input.bindKey(me.input.KEY.X,     "jump", true);
+
+		// start the game
+		me.state.change(me.state.PLAY);
+		// me.state.change(me.state.MENU);
+	}
+
+}; // jsApp
+
+/* the in game stuff*/
+var PlayScreen = me.ScreenObject.extend(
+{
+
+   onResetEvent: function() {
+		// play the audio track
+		// me.audio.playTrack("DST-InertExponent");
+
+		// load a level
+        // me.levelDirector.loadLevel("area01");
+		me.levelDirector.loadLevel("level01");
+
+        // add a default HUD to the game mngr
+        me.game.addHUD(0, 430, 640, 60);
+
+        // add a new HUD item
+        me.game.HUD.addItem("score", new ScoreObject(620, 10));
+
+        // make sure everyhting is in the right order
+        me.game.sort();
+	},
+
+
+	/* ---
+
+		action to perform when game is finished (state change)
+
+		---	*/
+	onDestroyEvent: function() {
+		// remove the HUD
+        me.game.disableHUD();
+
+		// stop the current audio track
+		me.audio.stopTrack();
+    }
+
+});
+
+/*----------------------
+
+    A title screen
+
+    ----------------------*/
+
+var TitleScreen = me.ScreenObject.extend({    // constructor
+    init: function() {
+        this.parent(true);
+
+        // title screen image
+        this.title = null;
+
+        this.font = null;
+        this.scrollerfont = null;
+        this.scrollertween = null;
+
+        this.scroller = "A SMALL STEP BY STEP TUTORIAL FOR GAME CREATION WITH MELONJS       ";
+        this.scrollerpos = 600;
+    },
+
+    // reset function
+    onResetEvent: function() {
+        if (this.title === null) {
+            // init stuff if not yet done
+            this.title = me.loader.getImage("title_screen");
+            // font to display the menu items
+            this.font = new me.BitmapFont("32x32_font", 32);
+            this.font.set("left");
+
+            // set the scroller
+            this.scrollerfont = new me.BitmapFont("32x32_font", 32);
+            this.scrollerfont.set("left");
+
+        }
+
+        // reset to default value
+        this.scrollerpos = 640;
+
+        // a tween to animate the arrow
+        this.scrollertween = new me.Tween(this).to({
+            scrollerpos: -2200
+        }, 10000).onComplete(this.scrollover.bind(this)).start();
+
+        // enable the keyboard
+        me.input.bindKey(me.input.KEY.ENTER, "enter", true);
+
+        // play something
+        me.audio.play("cling");
+
+    },
+
+    // some callback for the tween objects
+    scrollover: function() {
+        // reset to default value
+        this.scrollerpos = 640;
+        this.scrollertween.to({
+            scrollerpos: -2200
+        }, 10000).onComplete(this.scrollover.bind(this)).start();
+    },
+
+    // update function
+    update: function() {
+        // enter pressed ?
+        if (me.input.isKeyPressed('enter')) {
+            me.state.change(me.state.PLAY);
+        }
+        return true;
+    },
+
+    // draw function
+    draw: function(context) {
+        context.drawImage(this.title, 0, 0);
+
+        this.font.draw(context, "PRESS ENTER TO PLAY", 20, 240);
+        this.scrollerfont.draw(context, this.scroller, this.scrollerpos, 440);
+    },
+
+    // destroy function
+    onDestroyEvent: function() {
+        me.input.unbindKey(me.input.KEY.ENTER);
+
+        //just in case
+        this.scrollertween.stop();
+    }
+
+});
+//bootstrap :)
+window.onReady(function()
+{
+	jsApp.onload();
+});
